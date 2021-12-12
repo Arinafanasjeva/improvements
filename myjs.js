121adb4823e0d89cd9d450af0cf3961d2957a0d1
Arina Afanasjeva <arin0181@stud.kea.dk>
2021-12-12 14:27:20 +0100

    why quickit

--- myjs.js
+++ myjs.js
@@ -1,4 +1,28 @@
-function myFunction() {
-  const popup = document.getElementById("myPopup");
+function myFunction1() {
+  const popup = document.getElementById("myPopup1");
+  popup.classList.toggle("show");
+}
+function myFunction2() {
+  const popup = document.getElementById("myPopup2");
+  popup.classList.toggle("show");
+}
+function myFunction3() {
+  const popup = document.getElementById("myPopup3");
+  popup.classList.toggle("show");
+}
+function myFunction4() {
+  const popup = document.getElementById("myPopup4");
+  popup.classList.toggle("show");
+}
+function myFunction5() {
+  const popup = document.getElementById("myPopup5");
+  popup.classList.toggle("show");
+}
+function myFunction6() {
+  const popup = document.getElementById("myPopup6");
+  popup.classList.toggle("show");
+}
+function myFunction7() {
+  const popup = document.getElementById("myPopup7");
   popup.classList.toggle("show");
 }
