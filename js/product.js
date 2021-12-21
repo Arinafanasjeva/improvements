55eb781f4d7f9a25c242c93d0e254488d491c04e
raniajeanbeyh <rani0511@stud.kea.dk>
2021-12-21 08:07:40 +0200

    fixing js maybe

--- js/product.js
+++ js/product.js
@@ -38,3 +38,31 @@ function handleMeal(meal) {
   myClone.querySelector("p.price").textContent = meal.Price + ",00 DKK";
   document.querySelector("main").appendChild(myClone);
 }
+function myFunction1() {
+  const popup = document.getElementById("myPopup1");
+  popup.classList.toggle("show");
+}
+function myFunction2() {
+  const popup = document.getElementById("myPopup2");
+  popup.classList.toggle("show");
+}
+function myFunction3() {
+  const popup = document.getElementById("myPopup3");
+  popup.classList.toggle("show");
+}
+function myFunction4() {
+  const popup = document.getElementById("myPopup4");
+  popup.classList.toggle("show");
+}
+function myFunction5() {
+  const popup = document.getElementById("myPopup5");
+  popup.classList.toggle("show");
+}
+function myFunction6() {
+  const popup = document.getElementById("myPopup6");
+  popup.classList.toggle("show");
+}
+function myFunction7() {
+  const popup = document.getElementById("myPopup7");
+  popup.classList.toggle("show");
+}
